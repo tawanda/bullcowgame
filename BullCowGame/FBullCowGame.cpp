7755a404fb36adea6d764972d2bd5f761da27af5
Tawanda Minya <tminya@gmail.com>
2018-02-06 19:44:22 +0200

    Fixed bugs

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -5,24 +5,35 @@ void FBullCowGame::Reset() {
 	constexpr int32 MAX_TRIES = 8;
 	this->MyMaxTries = MAX_TRIES;
 
-	const FString HIDDEN_WORD = "planet";
+	const FString HIDDEN_WORD = "ant";
+	this->MyHiddenWord = HIDDEN_WORD;
 
 	this->MyCurrentTry = 1;
 	return;
 }
 
-FBullCowCount FBullCowGame::SubmitGuess(FString) {
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess) {
 
-	MyCurrentTry++;
+	this->MyCurrentTry++;
 	FBullCowCount BullCowCount;
 
-	int32 HiddenWordLength = MyHiddenWord.length();
+	int32 HiddenWordLength = this->MyHiddenWord.length();
 
-	for (int32 i = 0; i < HiddenWordLength; i++) {
+	int32 GuessWordLength = 0;
+	
+	// Fix the bug of out of range when Guess length is bigger than word length
+	(Guess.length() > HiddenWordLength) ? (GuessWordLength = HiddenWordLength) : (GuessWordLength = Guess.length());
 
-		for (int32 j = 0; j < HiddenWordLength; j++) {
+	for (int32 i = 0; i < GuessWordLength; i++) {
+		if (Guess[i] == this->MyHiddenWord[i]) {
+			BullCowCount.Bulls++; 
 		}
 
+		for (int32 j = 0; j < HiddenWordLength; j++) {
+			if (Guess[i] == this->MyHiddenWord[j] && i!=j) { 
+				BullCowCount.Cows++; 
+			}
+		}
 	}
 
 	return BullCowCount;
@@ -30,8 +41,8 @@ FBullCowCount FBullCowGame::SubmitGuess(FString) {
 
 FBullCowGame::FBullCowGame() { Reset(); }
 
-int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
-int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
-void FBullCowGame::SetCurrentTry(int32 TryNumber) { MyCurrentTry = TryNumber; }
+int32 FBullCowGame::GetMaxTries() const { return this->MyMaxTries; }
+int32 FBullCowGame::GetCurrentTry() const { return this->MyCurrentTry; }
+void FBullCowGame::SetCurrentTry(int32 TryNumber) { this->MyCurrentTry = TryNumber; }
 bool FBullCowGame::IsGameWon() const { return false; }
 bool FBullCowGame::CheckGuessValidity(FString) const { return false; }
